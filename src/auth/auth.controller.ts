e36f4777aa3b41577dfbbdf3341f006d329720ae
yntymakdev <kubanychevyntymak21@gmail.com>
2025-06-18 22:38:08 +0600

    qurad/superviser/operator

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -8,6 +8,7 @@ import {
 } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthDto } from './dto/auth.dto';
+import { RefreshTokenDto } from './dto/refresh-token.dto';
 
 @Controller('auth')
 export class AuthController {
@@ -17,5 +18,17 @@ export class AuthController {
   @Post('register')
   async register(@Body() dto: AuthDto) {
     return this.authService.register(dto);
+  }
+  @UsePipes(new ValidationPipe())
+  @HttpCode(200)
+  @Post('login')
+  async login(@Body() dto: AuthDto) {
+    return this.authService.login(dto);
+  }
+  @UsePipes(new ValidationPipe())
+  @HttpCode(200)
+  @Post('login/access-token')
+  async getnNewTokens(@Body() dto: RefreshTokenDto) {
+    return this.authService.getNewTokens(dto.refreshToken);
   }
 }
