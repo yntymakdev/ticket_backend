1e0daaaef990d90fdef945a331f895d14946925a
yntymakdev <kubanychevyntymak21@gmail.com>
2025-06-18 19:04:28 +0600

    getbyId/email

---
+++ src/user/user.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma.service';
+
+@Injectable()
+export class UserService {
+  constructor(private prisma: PrismaService) {}
+  async getById(id: string) {
+    return this.prisma.user.findUnique({
+      where: {
+        id,
+      },
+      include: {
+        favorites: true,
+      },
+    });
+  }
+  async getByEmail(email: string) {
+    return this.prisma.user.findUnique({
+      where: {
+        email,
+      },
+      include: {
+        favorites: true,
+      },
+    });
+  }
+}
